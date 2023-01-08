e0b758d4ccc5afa0d569d8cbf72f88e82d850bb8
David Berlin <david@stackbit.com>
2023-01-08 12:21:35 +0200

    move to custom content source

--- content-source.ts
+++ content-source.ts
@@ -1,16 +1,37 @@
+import _ from "lodash";
 import path from "path";
+import crypto from "crypto";
 import {
   ContentSourceOptions,
   SanityContentSource,
 } from "@stackbit/cms-sanity";
 import * as ContentSourceTypes from "@stackbit/types";
+import { Model, Logger } from "@stackbit/types";
+import { ContextualDocument } from "@stackbit/cms-sanity/dist/sanity-document-converter";
 
 export class LocalizedSanityContentSource extends SanityContentSource {
   private languageFile: string;
+  private localizedModels: string[];
+  private logger: Logger;
 
   constructor(options: ContentSourceOptions & { languageFile: string }) {
     super(options);
     this.languageFile = path.join(options.rootPath, options.languageFile);
+  }
+
+  async init(options) {
+    this.logger = options.logger;
+    return super.init(options);
+  }
+
+  async reset(): Promise<void> {
+    await super.reset();
+
+    const sanitySchema = await this.getSanitySchema();
+    this.localizedModels =
+      sanitySchema.models
+        ?.filter((model) => model.i18n)
+        .map((model) => model.name) ?? [];
   }
 
   async getLocales(): Promise<ContentSourceTypes.Locale[]> {
@@ -25,8 +46,113 @@ export class LocalizedSanityContentSource extends SanityContentSource {
     });
   }
 
+  async getModels(): Promise<Model[]> {
+    const models = await super.getModels();
+    return models.map((model) => {
+      if (this.localizedModels.includes(model.name)) {
+        return {
+          ...model,
+          localized: true,
+          fields: [
+            ...model.fields,
+            {
+              type: "string",
+              name: "__i18n_lang",
+              label: "lang",
+              hidden: true,
+            },
+            {
+              type: "reference",
+              name: "__i18n_base",
+              label: "Base translation",
+              models: [model.name],
+            },
+          ],
+        };
+      }
+      return model;
+    });
+  }
+
+  convertDocuments(options) {
+    const documents = super.convertDocuments(options);
+    return documents.map((document) => {
+      if (this.localizedModels.includes(document.modelName)) {
+        const sanitySourceDocument = document as ContextualDocument;
+        const sanityDocument = (sanitySourceDocument.context.draftDocument ??
+          sanitySourceDocument.context.publishedDocument)!;
+        const result = {
+          ...document,
+          locale: sanityDocument.__i18n_lang || "en-US",
+        };
+        if (sanityDocument.__i18n_base) {
+          result.fields.__i18n_base = {
+            type: "reference",
+            refId: sanityDocument.__i18n_base._ref,
+            refType: "document",
+          };
+        }
+        return result;
+      }
+      return document;
+    });
+  }
+
+  async updateDocument(options) {
+    const { document, operations, userContext } = options;
+    if (
+      this.localizedModels.includes(document.modelName) &&
+      operations.length === 1 &&
+      (operations[0].opType === "set" || operations[0].opType === "unset") &&
+      operations[0].fieldPath[0] === "__i18n_base"
+    ) {
+      const op = operations[0];
+      const opField = op.field;
+
+      const userClient = this.getApiClientForUser({ userContext });
+      const sanitySourceDocument = document as ContextualDocument;
+      const sanityDocument = (sanitySourceDocument.context.draftDocument ??
+        sanitySourceDocument.context.publishedDocument)!;
+
+      let updatedDocument;
+      if (op.opType === "set") {
+        updatedDocument = {
+          ...sanityDocument,
+          __i18n_base: {
+            _ref: opField.refId,
+            _strengthenOnPublish: {},
+            _type: "reference",
+            _weak: true,
+          },
+          _id:
+            "drafts." + opField.refId + "__i18n_" + sanityDocument.__i18n_lang,
+        };
+      } else {
+        updatedDocument = {
+          ...sanityDocument,
+          __i18n_base: null,
+          _id: crypto.randomBytes(16).toString("hex"),
+        };
+      }
+      if (sanityDocument._id !== updatedDocument._id) {
+        const transaction = userClient
+          .transaction()
+          .createIfNotExists(updatedDocument)
+          .delete(sanityDocument._id);
+
+        await transaction.commit({ visibility: "async" });
+      }
+      return {
+        document,
+        id: updatedDocument._id,
+      };
+    }
+    return super.updateDocument(options);
+  }
+
   async createDocument(options) {
-    if (options.model.name === "post" && options.locale) {
+    if (this.localizedModels.includes(options.model.name) && options.locale) {
+      this.logger.debug("create document with locale", options.locale);
       return super.createDocument({
         ...options,
         updateOperationFields: {
--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,6 +1,6 @@
 import { LocalizedSanityContentSource } from "./content-source";
 import { RawConfig } from "@stackbit/sdk";
-import { ContextualDocument } from "@stackbit/cms-sanity/dist/sanity-document-converter";
+import _ from "lodash";
 
 const stackbitConfig: RawConfig = {
   stackbitVersion: "~0.6.0",
@@ -21,41 +21,54 @@ const stackbitConfig: RawConfig = {
   models: {
     post: { type: "page", urlPath: "/posts/{slug}" },
   },
-  mapModels({ models }) {
-    return models.map((model) => {
-      if (model.name === "post") {
-        return {
-          ...model,
-          localized: true,
-          fields: [
-            ...model.fields,
-            {
-              type: "string",
-              name: "__i18n_lang",
-              label: "lang",
-              hidden: true
-            },
-          ],
-        };
-      }
-      return model;
-    });
-  },
-  mapDocuments({ documents, models }) {
-    return documents.map((document) => {
-      if (document.modelName === "post") {
-        const sanitySourceDocument = document as ContextualDocument;
-        const sanityDocument = (sanitySourceDocument.context.draftDocument ??
-          sanitySourceDocument.context.publishedDocument)!;
-        console.log(sanityDocument);
-        return {
-          ...document,
-          locale: sanityDocument.__i18n_lang || "en-US",
-        };
-      }
-      return document;
-    });
-  },
+  // mapModels({ models }) {
+  //   return models.map((model) => {
+  //     if (model.name === "post") {
+  //       return {
+  //         ...model,
+  //         localized: true,
+  //         fields: [
+  //           ...model.fields,
+  //           {
+  //             type: "string",
+  //             name: "__i18n_lang",
+  //             label: "lang",
+  //             hidden: true,
+  //           },
+  //           {
+  //             type: "reference",
+  //             name: "__i18n_base",
+  //             label: "Base translation",
+  //             models: ["post"],
+  //           },
+  //         ],
+  //       };
+  //     }
+  //     return model;
+  //   });
+  // },
+  // mapDocuments({ documents, models }) {
+  //   return documents.map((document) => {
+  //     if (document.modelName === "post") {
+  //       const sanitySourceDocument = document as ContextualDocument;
+  //       const sanityDocument = (sanitySourceDocument.context.draftDocument ??
+  //         sanitySourceDocument.context.publishedDocument)!;
+  //       const result = {
+  //         ...document,
+  //         locale: sanityDocument.__i18n_lang || "en-US",
+  //       };
+  //       if (sanityDocument.__i18n_base) {
+  //         result.fields.__i18n_base = {
+  //           type: "reference",
+  //           refId: sanityDocument.__i18n_base._ref,
+  //           refType: "document",
+  //         };
+  //       }
+  //       return result;
+  //     }
+  //     return document;
+  //   });
+  // },
   siteMap({ documents }) {
     return [
       {
@@ -69,7 +82,7 @@ const stackbitConfig: RawConfig = {
         .map((document) => {
           const slugField = document.fields.slug;
           const slugValue =
-            slugField.type === "slug" && slugField.localized !== true
+            slugField?.type === "slug" && slugField.localized !== true
               ? slugField.value
               : "";
           return {
