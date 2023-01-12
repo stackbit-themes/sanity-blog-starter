fcb9c0fffcfc7e0398f6bc30c400aabafdbbc312
David Berlin <david@stackbit.com>
2023-01-12 10:52:02 +0200

    Hide avatar for empty author

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -4,6 +4,9 @@ type Props = {
 }
 
 const Avatar = ({ name, picture }: Props) => {
+  if (!name) { 
+    return null;
+  }
   return (
     <div className="flex items-center">
       <img data-sb-field-path=".picture" src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
